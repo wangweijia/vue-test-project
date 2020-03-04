a0410687c1c1691bbf56173686db61d86d9b6a39
王唯佳 <wqywangweijia@gmail.com>
2020-03-05 01:51:12 +0800

    普通文字识别添加

--- src/components/cloud_baidu/imgToTxt.vue
+++ src/components/cloud_baidu/imgToTxt.vue
@@ -1,43 +1,114 @@
 <template>
   <div class="rootBox" v-loading="loading" >
+    <div>
+      要选择正确的解析格式
+    </div>
+    <el-radio-group v-model="imageToTxtType">
+      <el-radio v-for="(item, index) in analysisOpts" :key="index" :label="item.value">{{ item.name }}</el-radio>
+    </el-radio-group>
     <el-upload
       class="upload-demo"
       drag
-      action="http://118.190.162.218:9901/imgeUpdate"
+      :limit="1"
+      :action="updateAction"
       :on-error="error"
       :before-upload="beforeUpload"
       :on-success="success"
       multiple>
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件</div>
+      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，限制大小为2兆</div>
     </el-upload>
     <div>
       <template v-for="(item, index) in successFiles" >
-        <a :key="index" :href="item.result_data" >解析成功：{{item.name}}，点击下载</a>
+        <a v-if="item.imageToTxtType === ImageToTxtType.Excel" :key="index" :href="item.result_data" >解析成功：{{item.name}}，点击下载</a>
+        <div 
+        v-else-if="item.imageToTxtType === ImageToTxtType.OnlyTxt" 
+        :key="index"
+        @click="()=>showDialog(item)"
+        >解析成功：{{item.name}}，点击查看数据</div>
       </template>
     </div>
+
+    <el-dialog
+      title="解析结果"
+      :visible.sync="dialogVisible"
+      width="80%" >
+      <!-- <span>解析结果</span> -->
+      <pre>{{ currentTxtInfo.words_result }}</pre>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 
+const ImageToTxtType = {
+  // 普通文字
+  OnlyTxt: 1,
+  // 表格
+  Excel: 2
+}
+
 export default {
   data() {
     return {
+      ImageToTxtType,
       successFiles: [],
       loading: false,
+      imageToTxtType: ImageToTxtType.OnlyTxt,
+      dialogVisible: false,
+      currentTxtInfo: {}
+    }
+  },
+  computed: {
+    analysisOpts() {
+      return [
+        {
+          name: '普通文字',
+          value: ImageToTxtType.OnlyTxt
+        }, {
+          name: '表格',
+          value: ImageToTxtType.Excel
+        }
+      ]
+    },
+    updateAction() {
+      switch (this.imageToTxtType) {
+        case ImageToTxtType.OnlyTxt:
+          return 'http://118.190.162.218:9901/onlyText';
+        case ImageToTxtType.Excel:
+          return 'http://118.190.162.218:9901/imgeUpdate';
+        default:
+          return '';
+      }
     }
   },
   methods: {
     success(response, file, fileList) {
       console.log(response, file, fileList);
-      const { result } = response;
-      const { name } = file;
-      this.successFiles.push({
-        ...result,
-        name
-      });
+
+      if (this.imageToTxtType === ImageToTxtType.OnlyTxt) {
+        const { words_result } = response;
+        const { name } = file;
+        this.successFiles.push({
+          words_result,
+          name,
+          imageToTxtType: this.imageToTxtType
+        });
+      }
+      if (this.imageToTxtType === ImageToTxtType.Excel) {
+        const { result } = response;
+        const { name } = file;
+        this.successFiles.push({
+          ...result,
+          name,
+          imageToTxtType: this.Excel
+        });
+      }
+
       this.loading = false;
     },
     beforeUpload() {
@@ -46,6 +117,10 @@ export default {
     error(err) {
       this.loading = false;
       this.$notify(err);
+    },
+    showDialog(item) {
+      this.dialogVisible = true;
+      this.currentTxtInfo = item;
     }
   }
 }
@@ -58,6 +133,10 @@ export default {
   flex-direction: column;
   align-items: center;
   margin: 10px;
+}
+
+.upload-demo {
+  margin-top: 10px;
 }
 
 </style>
