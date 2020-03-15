f9ca006572985cc0788b61cf95bf76684ff013b9
王唯佳 <wqywangweijia@gmail.com>
2020-03-15 21:36:46 +0800

    提示加载速度

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,36 @@
+const path = require('path');
+
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
     devServer: {
         port: 9900,     // 端口
         disableHostCheck: true,
     },
+    configureWebpack:{
+        resolve:{
+          alias:{
+            '@':path.resolve(__dirname, './src'),
+            '@i':path.resolve(__dirname, './src/assets'),
+          } 
+        },
+        plugins: [
+          new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+          
+          // 下面是下载的插件的配置
+          new CompressionWebpackPlugin({
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            threshold: 10240,
+            minRatio: 0.8
+          }),
+          new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 5, 
+            minChunkSize: 100
+          })
+        ]
+    }
 };
