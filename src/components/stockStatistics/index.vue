6f4d994681122a8ba0f5471feb4d91f9d05450ec
王唯佳 <wqywangweijia@gmail.com>
2020-03-15 21:57:34 +0800

    添加头

--- src/components/stockStatistics/index.vue
+++ src/components/stockStatistics/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="page" >
+    <div>
+      by wwj
+    </div>
     <h1>市场情绪</h1>
     <Emotion/>
 
     <h1>北向资金</h1>
     <NorthwardCapital/>
-    
+
     <h1>涨跌停家数</h1>
     <LimitUpDownCount/>
   </div>
